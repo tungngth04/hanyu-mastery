2e3ea4a57a1592810407e4f43046eab3f3134005
tungvhn <140341762+tungngth04@users.noreply.github.com>
2026-03-11 17:28:23 +0700

    💄 feat: add dropdown profile ui

--- fe/src/components/organisms/header/index.tsx
+++ fe/src/components/organisms/header/index.tsx
@@ -1,7 +1,13 @@
-import { Bell, BellDot, Search } from "lucide-react";
+"use client";
+import { Divider } from "antd";
+import { BellDot, LogOut, Search, User } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
   return (
     <header className="flex items-center px-6 h-16 bg-white/80 shadow-md sticky top-0 z-50 border-b border-slate-200 backdrop-blur-md">
       <div className="relative flex-1 min-w-0 mr-2 ">
@@ -18,14 +24,49 @@ const Header = () => {
           <p className="text-sm font-bold ">Học viên</p>
           <p className="text-slate-500 text-xs">HSK 3</p>
         </div>
-        <div>
+        <div className="relative">
           <Image
             src="https://i.pravatar.cc/100?u=1"
             alt="avatar"
             width={40}
             height={40}
+            onClick={() => setOpen(!open)}
             className="rounded-full cursor-pointer"
           />
+
+          <div
+            className={`absolute -right-3 mt-5 w-56 bg-purple-300 text-white rounded-2xl shadow-lg transition-all duration-800 ${
+              open
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-3 pointer-events-none"
+            }`}
+          >
+            <div className="absolute -top-2 right-6 w-4 h-4 bg-purple-300 rotate-45"></div>
+            <div className="pt-4 pb-2 font-semibold text-center text-lg">
+              Loan Vu
+            </div>
+
+            <div className="px-6">
+              <Divider style={{ margin: 0, borderColor: "white" }} />
+            </div>
+
+            <button
+              className="flex items-center gap-3 px-6 py-3 w-full hover:bg-purple-400 transition cursor-pointer"
+              onClick={() => router.push("/profile")}
+            >
+              <User size={18} />
+              Hồ sơ cá nhân
+            </button>
+
+            <div className="px-6">
+              <Divider style={{ margin: 0, borderColor: "white" }} />
+            </div>
+
+            <button className="flex items-center gap-3 px-6 py-3 w-full hover:bg-purple-400 transition rounded-b-2xl cursor-pointer">
+              <LogOut size={18} />
+              Đăng xuất
+            </button>
+          </div>
         </div>
       </div>
     </header>
